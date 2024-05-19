4de6c178b57f99a1a8568162b54db61323ffaa98
falcaodiogo <falcao.diogo@ua.pt>
2024-05-19 01:13:58 +0100

    added map design and key points

--- app/src/main/java/ua/deti/pt/phoneapp/ui/components/map/MapComponent.kt
+++ app/src/main/java/ua/deti/pt/phoneapp/ui/components/map/MapComponent.kt
@@ -1,21 +1,69 @@
 package ua.deti.pt.phoneapp.ui.components.map
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MarkerComposable
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import ua.deti.pt.phoneapp.R
 
 @Composable
 fun MapScreen() {
-    val atasehir = LatLng(40.9971, 29.1007)
+    val aveiro = LatLng(40.638076, -8.653603)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(atasehir, 15f)
+        position = CameraPosition.fromLatLngZoom(aveiro, 15f)
     }
+    val locationMap = LatLng(40.638654, -8.655872)
+    val locationMap2 = LatLng(40.639322, -8.651495)
+    val locationMap3 = LatLng(40.636694, -8.653297)
+    val locationMap4 = LatLng(40.633362, -8.658463)
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
-    )
+    ) {
+        MarkerComposable(
+            state = MarkerState(position = locationMap),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.locationpoint),
+                contentDescription = "",
+                modifier = Modifier.size(50.dp)
+            )
+        }
+        MarkerComposable(
+            state = MarkerState(position = locationMap2),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.locationpoint),
+                contentDescription = "",
+                modifier = Modifier.size(50.dp)
+            )
+        }
+        MarkerComposable(
+            state = MarkerState(position = locationMap3),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.locationpoint),
+                contentDescription = "",
+                modifier = Modifier.size(50.dp)
+            )
+        }
+        MarkerComposable(
+            state = MarkerState(position = locationMap4),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.locationpoint),
+                contentDescription = "",
+                modifier = Modifier.size(50.dp)
+            )
+        }
+    }
 }
--- app/src/main/java/ua/deti/pt/phoneapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/ua/deti/pt/phoneapp/ui/screens/HomeScreen.kt
@@ -2,15 +2,21 @@ package ua.deti.pt.phoneapp.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ua.deti.pt.phoneapp.R
 import ua.deti.pt.phoneapp.ui.components.map.MapScreen
@@ -25,15 +31,15 @@ fun HomeScreen() {
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.matchParentSize()
         )
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(modifier = Modifier.padding(24.dp).padding(top = 124.dp), horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
                 text = stringResource(id = R.string.home),
                 color = Color.White,
                 fontSize = 36.sp,
             )
-        }
-        Box(modifier = Modifier.fillMaxSize()) {
-            MapScreen()
+            Box(modifier = Modifier.height(564.dp).padding(top = 100.dp).clip(shape = RoundedCornerShape(16.dp))) {
+                MapScreen()
+            }
         }
     }
 }
