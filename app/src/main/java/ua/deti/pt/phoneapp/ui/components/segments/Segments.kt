eb360df444cd36836231021919095351106a439c
falcaodiogo <falcao.diogo@ua.pt>
2024-05-19 12:35:12 +0100

    notifications page static frontend

--- app/src/main/java/ua/deti/pt/phoneapp/ui/components/segments/Segments.kt
+++ app/src/main/java/ua/deti/pt/phoneapp/ui/components/segments/Segments.kt
@@ -1,18 +1,46 @@
 package ua.deti.pt.phoneapp.ui.components.segments
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Segments() {
-    Box(modifier = Modifier.background(Color.White).height(60.dp).width(60.dp)) {
-        Text(text = "oi")
+    Column() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(90.dp)
+                .clip(shape = RoundedCornerShape(16.dp))
+                .background(Color(0xFFe8e4cc))
+        ) {
+            Row(modifier = Modifier.align(Alignment.CenterStart).padding(start = 16.dp).fillMaxWidth()) {
+                Box(
+                    modifier = Modifier.size(34.dp).clip(CircleShape).background(Color(0xFF9fae41)).align(Alignment.CenterVertically)
+                )
+                Column(modifier = Modifier.align(Alignment.CenterVertically).padding(start = 14.dp).fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+                    Text(text = "You walked more than 2km today!", color = Color.Black)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(text = "Click here to see more", color = Color(0xFF4A4739))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/ua/deti/pt/phoneapp/ui/screens/NotificationsScreen.kt
+++ app/src/main/java/ua/deti/pt/phoneapp/ui/screens/NotificationsScreen.kt
@@ -1,18 +1,26 @@
 package ua.deti.pt.phoneapp.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ua.deti.pt.phoneapp.R
@@ -20,6 +28,7 @@ import ua.deti.pt.phoneapp.ui.components.segments.Segments
 
 @Composable
 fun NotificationsScreen() {
+    val segmentsCount = 3
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(id = R.drawable.notifications_background),
@@ -27,15 +36,39 @@ fun NotificationsScreen() {
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.matchParentSize()
         )
-        Column(modifier = Modifier.padding(24.dp).padding(top = 124.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = stringResource(id = R.string.notifications),
-                color = Color.White,
-                fontSize = 36.sp,
-            )
-            Box(modifier = Modifier.padding(top = 16.dp)) {
-                Segments()
-                Text(text = "estava a fazer as tabs das notificações, mas falta primeiro conectar com a BD ou API acho")
+        Column(modifier = Modifier.padding(24.dp).padding(top = 100.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(modifier = Modifier.padding(bottom = 40.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                     text = segmentsCount.toString(),
+                     color = Color.White,
+                     fontSize = 52.sp,
+                 )
+                Text(
+                    text = stringResource(id = R.string.notifications),
+                    color = Color.White,
+                    fontSize = 24.sp,
+                )
+            }
+            Column(
+                modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                for (i in 1..segmentsCount) {
+                    Segments()
+                    if (i < segmentsCount) {
+                        Spacer(modifier = Modifier.height(4.dp))
+                    }
+                }
+                Spacer(modifier = Modifier.size(64.dp))
+                Box(modifier = Modifier.fillMaxWidth().height(140.dp).clip(shape = RoundedCornerShape(16.dp))) {
+                    Image(
+                        painter = painterResource(id = R.drawable.plannedcard),
+                        contentDescription = "Background",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.matchParentSize()
+                    )
+                    Text(text = "Planned\nExercises", fontSize = 36.sp, textAlign = TextAlign.Center)
+                }
             }
         }
     }
