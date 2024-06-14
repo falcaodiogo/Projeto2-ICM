f8e371ecef51e2caf0abbae0ac773de00ecdae6f
zegameiro <jose.mcgameiro@ua.pt>
2024-06-14 12:57:22 +0100

    Added dependencies to the build gradle files to include Health Services

--- wearOS_app/app/build.gradle.kts
+++ wearOS_app/app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -22,6 +25,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -63,4 +67,24 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    // Hilt dependency injection
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-work:1.2.0")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
+    // Wear
+    implementation("androidx.wear:wear:1.3.0")
+
+    // Health Services
+    implementation("androidx.health:health-services-client:1.0.0-rc02")
+
+    // Bridge between Futures and Coroutines
+    implementation("com.google.guava:guava:33.2.1-android")
+    implementation("androidx.concurrent:concurrent-futures-ktx:1.2.0")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-service:2.8.2")
 }
--- wearOS_app/build.gradle.kts
+++ wearOS_app/build.gradle.kts
@@ -3,3 +3,15 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
+    }
+}
