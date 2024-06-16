630804c0a6b483f6334430c10613610df9c0e715
falcaodiogo <falcao.diogo@ua.pt>
2024-06-16 23:44:33 +0100

    New sleep graph

--- phone_app/app/src/main/java/ua/deti/pt/phoneapp/ui/screens/SleepScreen.kt
+++ phone_app/app/src/main/java/ua/deti/pt/phoneapp/ui/screens/SleepScreen.kt
@@ -1,111 +1,77 @@
 package ua.deti.pt.phoneapp.ui.screens
 
-import android.health.connect.datatypes.SleepSessionRecord
+import android.content.Context
 import android.os.Build
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import co.yml.charts.common.model.Point
-import ua.deti.pt.phoneapp.data.health.SleepSessionData
-import ua.deti.pt.phoneapp.data.health.SleepSessionViewModel
-import ua.deti.pt.phoneapp.ui.components.sleepchart.SingleLineChartWithGridLines
-import ua.deti.pt.phoneapp.ui.components.sleepchart.SleepSessionRow
-import java.util.UUID
-
-@Composable
-fun SleepSessionScreen(
-    permissions: Set<String>,
-    permissionsGranted: Boolean,
-    sessionsList: List<SleepSessionData>,
-    uiState: SleepSessionViewModel.UiState,
-    onInsertClick: () -> Unit = {},
-    onError: (Throwable?) -> Unit = {},
-    onPermissionsResult: () -> Unit = {},
-    onPermissionsLaunch: (Set<String>) -> Unit = {}
-) {
-
-    val errorId = rememberSaveable { mutableStateOf(UUID.randomUUID()) }
-
-    LaunchedEffect(uiState) {
-        if (uiState is SleepSessionViewModel.UiState.Uninitialized) {
-            onPermissionsResult()
-        }
-
-        if (uiState is SleepSessionViewModel.UiState.Error && errorId.value != uiState.uuid) {
-            onError(uiState.exception)
-            errorId.value = uiState.uuid
-        }
-    }
-
-    if (uiState != SleepSessionViewModel.UiState.Uninitialized) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            if (!permissionsGranted) {
-                item {
-                    Button(
-                        onClick = { onPermissionsLaunch(permissions) }
-                    ) {
-                        Text(text = "Request permissions")
-                    }
-                }
-            } else {
-                item {
-                    Button(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(48.dp)
-                            .padding(4.dp),
-                        onClick = {
-                            onInsertClick()
-                        }
-                    ) {
-                        Text(text = "Insert sleep session")
-                    }
-                }
-
-                items(sessionsList) { session ->
-                    SleepSessionRow(session)
-                }
-            }
-        }
-    }
-}
+import androidx.health.connect.client.records.SleepSessionRecord
+import com.jaikeerthick.composable_graphs.composables.line.LineGraph
+import com.jaikeerthick.composable_graphs.composables.line.model.LineData
+import com.jaikeerthick.composable_graphs.composables.line.style.LineGraphColors
+import com.jaikeerthick.composable_graphs.composables.line.style.LineGraphFillType
+import com.jaikeerthick.composable_graphs.composables.line.style.LineGraphStyle
+import com.jaikeerthick.composable_graphs.composables.line.style.LineGraphVisibility
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @Composable
-fun SleepScreen() {
+fun SleepScreen(
+    context: Context
+) {
+    val sessionRecords = rememberSaveable { mutableStateOf<List<SleepSessionRecord.Stage>>(emptyList()) }
+    val pointsData = rememberSaveable { mutableStateOf<List<LineData>>(emptyList()) }
+    var dataProcessed by remember { mutableStateOf(false) }
+    var startTimeSleep = remember { Instant.now() }
+    var endTimeSleep = remember { Instant.now() }
+    val stageMap = mapOf(
+        2f to "Sleeping",
+        1f to "Awake",
+        7f to "Awake in bed",
+        5f to "Deep sleep",
+        4f to "Light sleep",
+        3f to "Out of bed",
+        6f to "REM"
+    )
 
-    val sleepSessions = listOf<SleepSessionRecord>()
+    HealthConnectScreen(onStepsAndCaloriesUpdated = { steps, calories ->
+    }, onSleepDataUpdated = { sleepSessionRecords ->
+        sessionRecords.value = sleepSessionRecords
+        dataProcessed = false
+    })
 
-    val pointsData = sleepSessions.mapIndexed { index, sleepSessionRecord ->
-        Point(
-            x = index.toFloat(),
-            y = index.toFloat(),
-        )
+    if (!dataProcessed && sessionRecords.value.isNotEmpty()) {
+        val newPointsData = mutableListOf<LineData>()
+        val formatter = DateTimeFormatter.ofPattern("HH:mm").withZone(ZoneId.systemDefault())
+        for (record in sessionRecords.value) {
+            val formattedTime = formatter.format(record.startTime)
+            val value = when (record.stage) {
+                SleepSessionRecord.STAGE_TYPE_SLEEPING -> 2f
+                SleepSessionRecord.STAGE_TYPE_AWAKE -> 0f
+                SleepSessionRecord.STAGE_TYPE_AWAKE_IN_BED -> 7f
+                SleepSessionRecord.STAGE_TYPE_DEEP -> 5f
+                SleepSessionRecord.STAGE_TYPE_LIGHT -> 4f
+                SleepSessionRecord.STAGE_TYPE_OUT_OF_BED -> 3f
+                SleepSessionRecord.STAGE_TYPE_REM -> 6f
+                else -> 0f
+            }
+            val lineData = LineData(formattedTime, value)
+            newPointsData.add(lineData)
+        }
+        pointsData.value = newPointsData
+        dataProcessed = true
     }
 
     Column(
@@ -123,20 +89,40 @@ fun SleepScreen() {
         )
         Spacer(modifier = Modifier.padding(10.dp))
         Box(modifier = Modifier.padding(20.dp)) {
-            if (pointsData.isNotEmpty()) {
+            if (sessionRecords.value.isNotEmpty()) {
                 Row {
                     Text(modifier = Modifier.padding(top = 70.dp, start = 12.dp), text = "oi")
-                    Box(
+
+                    LineGraph(
                         modifier = Modifier
-                            .padding(24.dp)
-                            .padding(start = 8.dp)
-                    ) {
-                        SingleLineChartWithGridLines(pointsData = pointsData)
-                    }
+                            .padding(horizontal = 16.dp, vertical = 12.dp),
+                        data = pointsData.value,
+                        onPointClick = { value: LineData ->
+                            showToast(context, value.x, stageMap[value.y] ?: "On bed")
+                        },
+                        style = LineGraphStyle(
+                            visibility = LineGraphVisibility(
+                                isYAxisLabelVisible = false,
+                                isCrossHairVisible = true
+                            ),
+                            colors = LineGraphColors(
+                                lineColor = Color.White,
+                                pointColor = Color.Black,
+                                clickHighlightColor = Color.White,
+                                fillType = LineGraphFillType.Gradient(
+                                    brush = Brush.verticalGradient(listOf(Color.Black, Color.Gray))
+                                )
+                            )
+                        )
+                    )
                 }
             } else {
                 Text(text = "No sleep data available.", color = Color.White)
             }
         }
     }
+}
+
+fun showToast(context: Context, time: String, stage: String) {
+    Toast.makeText(context, "Time: $time, Stage: $stage", Toast.LENGTH_SHORT).show()
 }
