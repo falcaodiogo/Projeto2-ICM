236a8c993b2df66f9ed6d5cd3b9bb89ab570977c
zegameiro <jose.mcgameiro@ua.pt>
2024-06-17 03:06:36 +0100

    Added a Screens file for new navigation Method

---
+++ wearOS_app/app/src/main/java/ua/deti/pt/wearosapp/Screens.kt
@@ -0,0 +1,30 @@
+package ua.deti.pt.wearosapp
+
+import androidx.navigation.NavController
+import ua.deti.pt.wearosapp.ui.state.SummaryScreenState
+
+sealed class Screen(
+    val route: String
+) {
+    object Exercise : Screen("exercise")
+    object ExerciseNotAvailable : Screen("exerciseNotAvailable")
+    object PreparingExercise : Screen("preparingExercise")
+    object Summary : Screen("summaryScreen") {
+        fun buildRoute(summary: SummaryScreenState): String {
+            return "$route/${summary.averageHeartRate}/${summary.totalDistance}/${summary.totalCalories}/${summary.elapsedTime}"
+        }
+
+        val averageHeartRateArg = "averageHeartRate"
+        val totalDistanceArg = "totalDistance"
+        val totalCaloriesArg = "totalCalories"
+        val elapsedTimeArg = "elapsedTime"
+    }
+}
+
+fun NavController.navigateToTopLevel(screen: Screen, route: String = screen.route) {
+    navigate(route) {
+        popUpTo(graph.id) {
+            inclusive = true
+        }
+    }
+}
