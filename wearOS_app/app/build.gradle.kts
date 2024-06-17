a905e3daa1cc24d8ca6e38f1e9da93b1d86b3b22
zegameiro <jose.mcgameiro@ua.pt>
2024-06-17 15:34:50 +0100

    Added a id to the plugins in the inner module build gradle

--- wearOS_app/app/build.gradle.kts
+++ wearOS_app/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.roborazzi)
     id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
