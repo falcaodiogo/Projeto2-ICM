12a46e4e13afb797d0e1f885f3d36d475bfc7157
zegameiro <jose.mcgameiro@ua.pt>
2024-06-17 03:00:03 +0100

    Deleted all the navigation related files

--- wearOS_app/app/src/main/java/ua/deti/pt/wearosapp/ui/components/navbar/NavPath.kt
+++
@@ -1,5 +0,0 @@
-package ua.deti.pt.wearosapp.ui.components.navbar
-
-enum class NavPath {
-    HOME, NOTIFICATIONS, SETTINGS
-}
--- wearOS_app/app/src/main/java/ua/deti/pt/wearosapp/ui/components/navbar/NavTitle.kt
+++
@@ -1,7 +0,0 @@
-package ua.deti.pt.wearosapp.ui.components.navbar
-
-object NavTitle {
-    const val HOME = "Home"
-    const val NOTIFICATIONS = "Notifications"
-    const val SETTINGS = "Settings"
-}
