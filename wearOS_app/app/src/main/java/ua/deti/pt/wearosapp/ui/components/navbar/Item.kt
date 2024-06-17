46de68e45b21dd0aecccc6571b68949f597d21b5
zegameiro <jose.mcgameiro@ua.pt>
2024-06-17 03:00:28 +0100

    Deleted the component files

--- wearOS_app/app/src/main/java/ua/deti/pt/wearosapp/ui/components/navbar/Item.kt
+++
@@ -1,9 +0,0 @@
-package ua.deti.pt.wearosapp.ui.components.navbar
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-open class Item(
-    val path: String,
-    val title: String,
-    val icon: ImageVector
-)
