222558d19e7292ff06fd69b2742c4b362fb35970
zegameiro <jose.mcgameiro@ua.pt>
2024-06-19 00:22:08 +0100

    Delete useless files that complicated add exercise logic

--- phone_app/app/src/main/java/ua/deti/pt/phoneapp/ui/screens/MainScreen.kt
+++ phone_app/app/src/main/java/ua/deti/pt/phoneapp/ui/screens/MainScreen.kt
@@ -13,9 +13,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import ua.deti.pt.phoneapp.Auth.GoogleAuthUiClient
+import ua.deti.pt.phoneapp.database.daos.ExerciseDao
+import ua.deti.pt.phoneapp.database.daos.UserDao
 import ua.deti.pt.phoneapp.ui.components.navbar.BottomNavigationBar
 import ua.deti.pt.phoneapp.ui.components.navbar.NavigationScreens
-import ua.deti.pt.phoneapp.ui.events.DailyExercisesViewModel
 
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -25,12 +26,13 @@ fun MainScreen(
     onSignOut: () -> Unit,
     googleAuthUiClient: GoogleAuthUiClient,
     context: Context,
-    exercisesViewModel: DailyExercisesViewModel
+    userDao: UserDao,
+    exerciseDao: ExerciseDao
 ) {
     Scaffold(bottomBar = {
         BottomAppBar(
             containerColor = Color.Black,
             modifier = Modifier.height(120.dp)
         ) { BottomNavigationBar(navController = navController) }
-    }) { NavigationScreens(navController = navController, onSignOut, googleAuthUiClient, context, exercisesViewModel) }
+    }) { NavigationScreens(navController = navController, onSignOut, googleAuthUiClient, context, exerciseDao, userDao) }
 }
